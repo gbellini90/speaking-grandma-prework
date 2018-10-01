e9bcdb30d6e6475146901b12cb3fe4a836f37ad9
Gabrielle Bellini <gbellini90@gmail.com>
2018-10-01 15:26:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,5 @@
 def speak_to_grandma(string)
-  if string=="I LOVE YOU GRANDMA!"
-  return "I LOVE YOU TOO PUMPKIN!"
-  elsif string==string.upcase
-  return "NO, NOT SINCE 1938!"
-  elsif string==string.downcase
-  return "HUH?! SPEAK UP, SONNY!" 
-end
+
 end 
 
 
