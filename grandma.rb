273157f28b5e37896e7d573c57f20c665630ba7a
Gabrielle Bellini <gbellini90@gmail.com>
2018-10-01 15:23:21 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(string)
   elsif string.upcase
   return "NO, NOT SINCE 1938!"
   elsif string.downcase
-  return "HUH?! SPEAK UP, SONNY!"
+  return "HUH?! SPEAK UP, SONNY!""HUH?! SPEAK UP, SONNY!"
 end
 end 
 
